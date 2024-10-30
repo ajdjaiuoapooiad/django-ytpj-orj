1c9a384341818d868d595e385a2c05aab59f0221
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-10-30 11:54:09 +0900

    error channelModel

--- channel/models.py
+++ channel/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from taggit.managers import TaggableManager
+from core.models import user_derectory_path
+from ytpj import settings
 
-# Create your models here.
+User=settings.AUTH_USER_MODEL
+
+
+def user_derectory_path(instance,filename):
+    return 'user_{0}/{1}'.format(instance.user.id,filename)
+
+
+
+
+STATUS = (
+    ('Active','Active'),
+    ('Disabled','Disabled'),
+)
+
+
+
+class Channel(models.Model):
+    channel_art=models.ImageField(upload_to=user_derectory_path,default='static/images/channel-art.jpg')
+    image=models.ImageField(upload_to=user_derectory_path,default='static/images/user-image.png')
+    full_name=models.CharField(max_length=100)
+    channel_name=models.CharField(max_length=100)
+    description=models.TextField(null=True,blank=True)
+    keyword=TaggableManager
+    date=models.DateTimeField(auto_now_add=True)
+    status=models.CharField(choices=STATUS,max_length=100,default='Active')
+    user=models.OneToOneField(User,on_delete=models.SET_NULL,null=True,blank=True)
+    subscibers=models.ManyToManyField(User,related_name="user_subs")
+    verified=models.BooleanField(default=False)
+    
+    total_view=models.PositiveIntegerField(default=0)
+    business_email=models.EmailField(null=True,blank=True)
+    make_business_email_public=models.BooleanField(default=False)
+    business_location=models.CharField(null=True,blank=True,max_length=100)
+    make_business_location_public=models.BooleanField(default=False)
+    
+    
+    website=models.URLField(default='https://www.udemy.com/home')
+    instagram=models.URLField(default='https://www.udemy.com/home')
+    facebook=models.URLField(default='https://www.udemy.com/home')
+    twitter=models.URLField(default='https://www.udemy.com/home')
+    
+    def __str__(self):
+        return self.channel_name
+    
