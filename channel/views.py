928e0ca76e18a61ed697ef33b4334a9caf225588
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-10-31 13:51:41 +0900

    profile error ok 🍷

--- channel/views.py
+++ channel/views.py
@@ -11,10 +11,18 @@ from django.contrib import messages
 @login_required
 def channel_profile(request,channel_name):
     channel = get_object_or_404(Channel,id=channel_name)
+    videos=Video.objects.filter(user=channel.user,visibility='public').order_by('-views')
     
+    try:
+        video_featured=Video.objects.get(featured=True,user=channel.user.id)
+    except:
+        video_featured = None
+        messages.warning(request,f'Only one video can featured!')
     
     context={
         'channel': channel,
+        'videos': videos,
+        'video_featured': video_featured,
     }
     return render(request,'channel/channel.html',context)
 
